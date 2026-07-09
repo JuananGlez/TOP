6e626a517141b2dc2b0ae5bd8b58d2f3bfaf3643
JuananGlez <juanantoniogl3@protonmail.com>
2026-07-09 11:07:04 +0200

    Started javascript course

---
+++ javascript/books.js
@@ -0,0 +1,16 @@
+function Book(name, author, pages, read) {
+    if (!new.target) {
+        throw Error("You must use the 'new' operator to call the constructor");
+    }
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function () {
+        console.log(this.name + " by " + this.author + ", " + this.pages + ", " + this.read);
+    };
+}
+
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295 pages", "not read yet")
+
+console.log(theHobbit.info()); // "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
