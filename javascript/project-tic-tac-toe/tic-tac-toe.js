f292fbdc8ee7f1ecd195f7fc2677885e1a582403
JuananGlez <juanantoniogl3@protonmail.com>
2026-07-23 10:10:21 +0200

    Project Tic Tac Toe started

---
+++ javascript/project-tic-tac-toe/tic-tac-toe.js
@@ -0,0 +1,30 @@
+const gameBoard = {
+    "b1": "",
+    "b2": "",
+    "b3": "",
+    "b4": "",
+    "b5": "",
+    "b6": "",
+    "b7": "",
+    "b8": "",
+    "b9": "",
+};
+const playerX = {
+
+};
+const player0 = {
+
+};
+
+(() => {
+    document.getElementById('b1').innerHTML = gameBoard.b1;
+
+
+})();
+
+// let idButton = document.getElementById('b1').id
+
+function clickPlay(id) {
+    gameBoard.  = "X";
+    console.log(id)
+}
