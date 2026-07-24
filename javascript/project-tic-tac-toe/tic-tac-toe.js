7f79dd154141904629ab9f6bf25fa7662f6b8d43
juanan <juanantoniogl3@protonmail.com>
2026-07-24 15:59:01 +0200

    Integrated startGame and resetGame to Game IIFE

--- javascript/project-tic-tac-toe/tic-tac-toe.js
+++ javascript/project-tic-tac-toe/tic-tac-toe.js
@@ -1,31 +1,45 @@
-
-const createPlayer = (name, mark) => {
-    return {
-        name,
-        mark
-    }
+const Gameboard = {
+    gameBoard: ["", "", "", "", "", "", "", "", ""],
+    players: [],
+    turn: [0, 1]
 }
+let start = document.querySelector('#start');
+let reset = document.querySelector('#reset');
+
 const Game = (() => {
-    let gameBoard = ["", "", "", "", "", "", "", "", "",];
-    let players = [
-        createPlayer(document.querySelector('#PlayerX').value, 'X'),
-        createPlayer(document.querySelector('#PlayerO').value, 'O')
-    ];
-    gameBoard.forEach(() => {
-        let button = document.createElement('button')
+    let createPlayer = (name, mark) => {
+        return {
+            name,
+            mark
+        }
+    }
 
-        document.querySelector('.gameBoard').append(button)
-
-    })
-    const startGame = () => {
-        console.log('hola')
-
+    let startGame = () => {
+        Gameboard.players = [
+            createPlayer(document.querySelector('#PlayerX').value, 'X'),
+            createPlayer(document.querySelector('#PlayerO').value, 'O')
+        ];
+        Gameboard.gameBoard.forEach((element, index) => {
+            let button = document.createElement('button')
+            button.id = index
+            document.querySelector('.gameBoard').append(button)
+        })
+        if (window.getComputedStyle(start).display == 'grid') {
+            start.style.display = 'none';
+            reset.style.display = 'grid';
+        }
     };
-    return { startGame, }
+
+    let resetGame = () => {
+        if (start.style.display == "none") {
+            start.style.display = 'grid'
+            reset.style.display = 'none';
+        }
+        document.querySelector('.gameBoard').replaceChildren();
+    };
+
+    return { startGame, resetGame };
 
 })();
 
-
 (() => { })();
-
-
